61fe6e1c0324276b0b2f7e6e20b911935899bb9d
instaRa <instara07@gmail.com>
2022-09-11 11:47:56 -0300

    Create essencials functions in CardGame.js components

--- sources/js/components/CardGame.js
+++ sources/js/components/CardGame.js
@@ -1,16 +1,33 @@
-function CardGame()
-{
+function CardGame() {
     const $deck = document.querySelectorAll('.card-game');
 
-    if($deck.length <= 0){
+    if ($deck.length <= 0) {
         return null;
     }
 
     $deck.forEach($card => {
-        $card.addEventListener('click', function(){
+        $card.flip = () => {
             $card.classList.toggle('-flip');
-        })
+        }
+
+        $card.hide = () => {
+            $card.classList.remove('-flip');
+        }
+
+        $card.show = () => {
+            $card.classList.add('-flip');
+        }
+
+        // Return a base64 string of element
+        $card.fingerprint = () => {
+            return btoa($card.innerHTML);
+        }
+        
+        // Checks if this card has been selected
+        $card.selected = false;
     });
+
+    return $deck;
 }
 
 export default CardGame;
